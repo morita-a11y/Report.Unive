68414a9b7e2418ca0f7a6ad921b3986180b31614
RenMorita <m.renking1002@gmail.com>
2025-02-12 13:58:02 +0900

    fix: remove gitignore as submodule

--- Information_network_application/Assignment3/game_server.c
+++ Information_network_application/Assignment3/game_server.c
@@ -1,112 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
+#include <unistd.h>
 #include <arpa/inet.h>
-#include <errno.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <time.h>
 
-#define BUFFER_SIZE BUFSIZ
+#define PORT 20030
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 1024
 
-ssize_t safe_write(int fd, const void *buf, size_t count) {
-    ssize_t written = 0, total_written = 0;
-    while (total_written < count) {
-        written = write(fd, (char *)buf + total_written, count - total_written);
-        if (written < 0) {
-            if (errno == EINTR) continue; // シグナル割り込みなら再試行
-            perror("write");
-            return -1;
-        }
-        total_written += written;
+typedef struct {
+    int fd;
+    char name[50];
+    int score;
+    int attempts;
+} Client;
+
+int secret_number;
+Client clients[MAX_CLIENTS];
+
+void reset_game() {
+    secret_number = rand() % 100 + 1;
+    printf("[SERVER] New secret number: %d\n", secret_number);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        clients[i].attempts = 0;
     }
-    return total_written;
 }
 
-int main (int argc, char *argv[]) {
-    int sockfd;
-    char buffer[BUFFER_SIZE];
-    char mesg[BUFFER_SIZE] = "Hello, world!\n";
-    char *portname = "10000";
-    char *servername = "localhost";
-    int error;
-    struct addrinfo hints, *res, *r;
-
-    if (argc >= 2) {
-        servername = argv[1];    /* 第1引数: サーバー名 */
-        if (argc >= 3) {
-            portname = argv[2];  /* 第2引数: ポート名 */
-            if (argc >= 4) {
-                snprintf(mesg, BUFFER_SIZE, "%s\n", argv[3]); /* 第3引数: 送信文字列 */
-            }
+void broadcast(int sender_fd, const char *message) {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].fd != -1 && clients[i].fd != sender_fd) {
+            send(clients[i].fd, message, strlen(message), 0);
         }
     }
+}
 
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = PF_UNSPEC;       /* IPv4 or IPv6 */
-    hints.ai_socktype = SOCK_STREAM;   /* TCP */
-    if ((error = getaddrinfo(servername, portname, &hints, &res))) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(error));
-        exit(1);
-    }
+void handle_client_message(int client_index, char *message) {
+    Client *client = &clients[client_index];
+    int guess = atoi(message);
+    client->attempts++;
 
-    for (r = res; r; r = r->ai_next) {
-        if ((sockfd = socket(r->ai_family, r->ai_socktype, r->ai_protocol)) < 0)
-            continue;
-        if (connect(sockfd, r->ai_addr, r->ai_addrlen) == 0)
-            break;
-        close(sockfd);
-        sockfd = -1;
+    char response[BUFFER_SIZE];
+    if (guess == secret_number) {
+        int points = 11 - client->attempts;
+        if (points < 1) points = 1;
+        client->score += points;
+        snprintf(response, BUFFER_SIZE, "[%s]: %d correct! You got %d points\n", client->name, guess, points);
+        send(client->fd, response, strlen(response), 0);
+        reset_game();
+    } else {
+        snprintf(response, BUFFER_SIZE, "[%s]: %s than %d, current points %d\n", client->name,
+                 (guess < secret_number) ? "Higher" : "Lower", guess, 10 - client->attempts);
+        send(client->fd, response, strlen(response), 0);
     }
-    freeaddrinfo(res);
-    if (sockfd < 0) {
-        perror("Failed to connect");
-        exit(1);
-    }
+}
 
-    fd_set read_fds;
+int main() {
+    srand(time(NULL));
+    reset_game();
+
+    int server_fd, new_socket, max_fd;
+    struct sockaddr_in address;
+    fd_set readfds;
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, MAX_CLIENTS);
     
-    for (;;) {
-        FD_ZERO(&read_fds);
-        FD_SET(STDIN_FILENO, &read_fds);
-        FD_SET(sockfd, &read_fds);
+    for (int i = 0; i < MAX_CLIENTS; i++) clients[i].fd = -1;
+    printf("[SERVER] Listening on port %d...\n", PORT);
+    
+    while (1) {
+        FD_ZERO(&readfds);
+        FD_SET(server_fd, &readfds);
+        max_fd = server_fd;
 
-        int max_fd = (sockfd > STDIN_FILENO) ? sockfd : STDIN_FILENO;
-        int activity = select(max_fd + 1, &read_fds, NULL, NULL, NULL);
-        if (activity < 0) {
-            if (errno == EINTR) continue; // シグナル割り込みなら再試行
-            perror("select");
-            break;
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (clients[i].fd > 0) {
+                FD_SET(clients[i].fd, &readfds);
+                if (clients[i].fd > max_fd) max_fd = clients[i].fd;
+            }
         }
 
-        // 標準入力からの入力をサーバーへ送信
-        if (FD_ISSET(STDIN_FILENO, &read_fds)) {
-            ssize_t bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-            if (bytes_read < 0) {
-                perror("read (stdin)");
-                break;
-            } else if (bytes_read == 0) {
-                printf("Standard input closed.\n");
-                break;
+        select(max_fd + 1, &readfds, NULL, NULL, NULL);
+        
+        if (FD_ISSET(server_fd, &readfds)) {
+            socklen_t addrlen = sizeof(address);
+            new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
+            printf("[SERVER] New connection accepted\n");
+            for (int i = 0; i < MAX_CLIENTS; i++) {
+                if (clients[i].fd == -1) {
+                    clients[i].fd = new_socket;
+                    strcpy(clients[i].name, "Player");
+                    clients[i].score = 0;
+                    clients[i].attempts = 0;
+                    send(new_socket, "Welcome! Guess a number between 1 and 100.\n", 44, 0);
+                    break;
+                }
             }
-            if (safe_write(sockfd, buffer, bytes_read) < 0) break;
         }
-
-        if (FD_ISSET(sockfd, &read_fds)) {
-            ssize_t bytes_received = read(sockfd, buffer, BUFFER_SIZE);
-            if (bytes_received < 0) {
-                perror("read (socket)");
-                break;
-            } else if (bytes_received == 0) {
-                printf("Server closed the connection.\n");
-                break;
+        
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (clients[i].fd != -1 && FD_ISSET(clients[i].fd, &readfds)) {
+                char buffer[BUFFER_SIZE];
+                int valread = read(clients[i].fd, buffer, BUFFER_SIZE);
+                if (valread <= 0) {
+                    close(clients[i].fd);
+                    clients[i].fd = -1;
+                    printf("[SERVER] A player disconnected\n");
+                } else {
+                    buffer[valread] = '\0';
+                    handle_client_message(i, buffer);
+                }
             }
-            if (safe_write(STDOUT_FILENO, buffer, bytes_received) < 0) break;
         }
     }
-
-    close(sockfd);
     return 0;
-}
+} 
